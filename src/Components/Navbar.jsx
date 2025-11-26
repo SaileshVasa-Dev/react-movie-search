b47a8b7e16cda814afc171964d6275a57d1214fd
Sailesh Vasa <sailesh.vasa11@gmail.com>
2025-11-26 14:10:32 +0530

    Navigation Changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,43 @@
 import React from "react";
 import Logo from "../Images/Movielogo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+  const location = useLocation();
+
+  // Detect which page is active
+  const activePage = location.pathname;
+
+  const linkBase = "text-2xl font-bold transition duration-200 cursor-pointer";
+  const inactive = "text-gray-300 hover:text-white/90";
+  const active = "text-white font-extrabold";
+
   return (
-    <div className="flex border h-[50px] items-center space-x-8 pl-3 py-4">
-      <img className="h- 20 w-20" src={Logo} alt="Movies Logo" />
-      <Link to="/" className="text-gray-300 text-2xl font-bold">
+    <div className="flex items-center space-x-8 px-5 py-3 bg-black/40 backdrop-blur-lg border-b border-white/10">
+      {/* Clickable Logo → Go to Movies */}
+      <Link to="/" className="cursor-pointer">
+        <img
+          className="h-10 w-10 hover:opacity-80 transition"
+          src={Logo}
+          alt="Movies Logo"
+        />
+      </Link>
+
+      {/* Movies Link */}
+      <Link
+        to="/"
+        className={`${linkBase} ${activePage === "/" ? active : inactive}`}
+      >
         Movies
       </Link>
-      <Link to="/watchlist" className="text-gray-300 text-2xl font-bold">
+
+      {/* Watchlist Link */}
+      <Link
+        to="/watchlist"
+        className={`${linkBase} ${
+          activePage === "/watchlist" ? active : inactive
+        }`}
+      >
         Watchlist
       </Link>
     </div>
--- src/Components/Pagenation.jsx
+++ src/Components/Pagenation.jsx
@@ -8,11 +8,13 @@ function Pagenation({ handlePrev, handleNext, pageNo }) {
                 items-center gap-6 shadow-xl"
     >
       <button onClick={handlePrev} className="px-8 hover:cursor-pointer">
-        <i className="fa-regular fa-circle-left"></i>
+        <i className="fa-regular fa-circle-left text-gray-300"></i>
       </button>
-      <div className="font-bold hover:cursor-pointer">{pageNo}</div>
+      <div className="font-bold hover:cursor-pointer text-gray-200 ">
+        {pageNo}
+      </div>
       <button onClick={handleNext} className="px-8 hover:cursor-pointer">
-        <i className="fa-regular fa-circle-right"></i>
+        <i className="fa-regular fa-circle-right text-gray-300"></i>
       </button>
     </div>
   );
